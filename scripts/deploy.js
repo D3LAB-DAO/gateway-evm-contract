350403137760f541fb1f3009036fca5fbb025c6e
mg12 <volibear1001@gmail.com>
2023-06-26 16:56:16 +0900

    chore: Added testnet v8s contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,9 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const deployer = (await ethers.getSigners())[0];
+  console.log("deployer address: ", deployer.address);
+
   const v8c = await ethers.deployContract("V8S");
   await v8c.waitForDeployment();
 
@@ -11,3 +14,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+// 0xFc6256A220AF6F544719bcc5deE58F6BF5CafD8b
